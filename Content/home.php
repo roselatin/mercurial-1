035cab7c3f087120fd155ad341821bd4900a7d71
frankgomendoza <frank_gomendoza@yahoo.com>
2015-09-26 11:55:47 +0800

    Added Commit Sample

--- Content/home.php
+++ Content/home.php
@@ -50,6 +50,99 @@
 
 </div>
 
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+        <li data-target="#myCarousel" data-slide-to="4"></li>
+        <li data-target="#myCarousel" data-slide-to="5"></li>
+        <li data-target="#myCarousel" data-slide-to="6"></li>
+        <li data-target="#myCarousel" data-slide-to="7"></li>
+        <li data-target="#myCarousel" data-slide-to="8"></li>
+        <li data-target="#myCarousel" data-slide-to="9"></li>
+        <li data-target="#myCarousel" data-slide-to="10"></li>
+        <li data-target="#myCarousel" data-slide-to="11"></li>
+        <li data-target="#myCarousel" data-slide-to="12"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="images/events/Animobiz.jpg" alt="Animo Biz">
+            <div class="carousel-caption">
+                <h3>Animo Biz</h3>
+                <p>Have a great product idea and want to test it out?
+                    Don't worry, BIZ is your chance! Visit www.bit.ly/AnimoBIZ !</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="images/events/Utour.jpg" alt="Unilever Tour">
+            <div class="carousel-caption">
+                <h3>U-TOUR 2015</h3>
+                <p>Unilever is coming to De La Salle University’s Henry Sy Grounds to showcase the different programmes they have this year that are especially for you!</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="images/events/IECEP.jpg" alt="IECEP Recruitment">
+            <div class="carousel-caption">
+                <h3>IECEP Recruitment</h3>
+                <p>Expand your connections and be part of something big!</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="images/events/PCB_seminar.jpg" alt="PCB Seminar">
+            <div class="carousel-caption">
+                <h3>PCB Seminar</h3>
+                <p>Learn the basics of PCB making from our very own Research and Development officers!
+                </p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="images/events/DLSU_jobexpo.png" alt="DLSU Job Expo">
+            <div class="carousel-caption">
+                <h3>DLSU Job Expo</h3>
+                <p>Are you ready to SHIFT YOUR GEARS for the DLSU Job Expo 2015?
+                    The list of participating companies and organizations may be viewed here: http://tinyurl.com/JESEP15List</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="images/events/DOTA2_Tournament.jpg" alt="ACCESS DOTA 2 League">
+            <div class="carousel-caption">
+                <h3>ACCESS DOTA 2 League</h3>
+                <p>Coming SOON! Stay tuned for updates!</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="images/events/Frosh_convocation.jpg" alt="Frosh Convocation">
+            <div class="carousel-caption">
+                <h3>Frosh Convocation</h3>
+                <p>Grab the opportunity to know your professors who will be guiding and teaching you throughout your whole college life.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+
 <?php include("footer.html");?>
 </body>
 
