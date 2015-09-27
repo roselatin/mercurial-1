cfac2c09e544c82b42e23ac050715831bfb9afd5
frankgomendoza <frank_gomendoza@yahoo.com>
2015-09-27 20:41:26 +0800

    Added Commit Sample

--- Content/home.php
+++ Content/home.php
@@ -17,39 +17,6 @@
 <?php include("header.html");?>
 <div class="wrapper">
 
-    <div class="container-fluid "id="content">
-        <button class="btn btn-4 btn-4b" style="font-family:Helvetica;" data-dismiss="modal">Close</button>
-
-        HOME
-    </div>
-    Electronics System Group
-    -Electronic Systems Engineering focuses on the integration of electronics, computers, and communication
-    technologies in many different types of systems.
-
-    Communications System Group
-    -Communication Systems provides comprehensive knowledge on current and emerging technologies in the
-    design and development of communications-based networks and systems. The program addresses advances in
-    electronics, computing, networking, and wireless technologies.
-
-    -Communications system engineering may involve developing or maintaining cable-based or wireless telephone
-    and voicemail sysems, group scheduling programs, webinars, Voice over Internet Protocol or automatic
-    call answering and routing systems.
-
-    Digital Signal Processing Group
-    -Digital Signal Processing, has emerged as an important technology for modern electronic systems. It is a
-    form of embedded design that is one of the newest and hottest fields, and is considered to be the workhouse
-    of choice for many computational-intensive applications. Modern applications include biomedical, communications,
-    imaging, speech, video signal, and multimedia signal processing.
-
-    Computing and Information Technology Group
-    - Computing and Information Technology includes software development, networking, and database design.
-
-
-    Embedded System Group
-    - Embedded Systems development ranges from microprocessor-based control systems, to system-on-chip (SoC) design,
-    and device software development.
-
-</div>
 
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
@@ -65,77 +32,127 @@
     </ol>
 
     <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
+    <div class="carousel-inner">
         <div class="item active">
-            <img src="images/events/Animobiz.jpg" alt="Animo Biz">
-            <div class="carousel-caption"
-                >
-                <h3>Animo Biz</h3>
-                <p>Have a great product idea and want to test it out?
-                    Don't worry, BIZ is your chance! Visit www.bit.ly/AnimoBIZ !</p>
+                <div class="container">
+                    <div class="slide-wrapper">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="slide-image">
+                                    <div class="thumbnail">
+                                        <img src="images/events/Animobiz.jpg" alt="Animo Biz">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="slide-content">
+                                <h1>Animo Biz</h1>
+                                <p>Have a great product idea and want to test it out?
+                                    Don't worry, BIZ is your chance! Visit:</p>
+                                <a href="a" class="btn-primary"> http://www.bit.ly/AnimoBIZ</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="item">
-            <img src="images/events/ARW_ECES.jpg" alt="ECES ARW 2015">
-            <div class="carousel-caption"
-                 >
-                <h3>ECES ARW 2015</h3>
-                <p>Annual Recruitment Week for ECES. Join ECES NOW!</p>
+            <div class="container">
+                <div class="slide-wrapper">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="slide-image">
+                                <div class="thumbnail">
+                                    <img src="images/events/ARW_ECES.jpg" alt="ECS ARW 2015">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="slide-content">
+                                <h1>ECES ARW 2015</h1>
+                                <p>Annual Recruitment Week for ECES. Join ECES NOW!</p>
+                                <a href="a" class="btn-primary"> Link</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="item">
-            <img src="images/events/Utour.jpg" alt="Unilever Tour">
-            <div class="carousel-caption"
-                 >
-                <h3>U-TOUR 2015</h3>
-                <p>Unilever is coming to De La Salle University’s Henry Sy Grounds to showcase the different programmes they have this year that are especially for you!</p>
+            <div class="container">
+                <div class="slide-wrapper">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="slide-image">
+                                <div class="thumbnail">
+                                    <img src="images/events/Utour.jpg" alt="Utour">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="slide-content">
+                                <h1>U-TOUR 2015</h1>
+                                <p>Unilever is coming to De La Salle University Henry Sy Grounds to showcase the
+                                    different programmes they have this year that are especially for you!</p>
+                                <a href="a" class="btn-primary"> Link</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="item">
-            <img src="images/events/IECEP.jpg" alt="IECEP Recruitment">
-            <div class="carousel-caption">
-                <h3>IECEP Recruitment</h3>
-                <p>Expand your connections and be part of something big!</p>
+            <div class="container">
+                <div class="slide-wrapper">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="slide-image">
+                                <div class="thumbnail">
+                                    <img src="images/events/IECEP.jpg" alt="IECEP">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="slide-content">
+                                <h1>IECEP Recruitment</h1>
+                                <p>Expand your connections and be part of something big!</p>
+                                <a href="a" class="btn-primary"> Link</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="item">
-            <img class="img-responsive center-block" src="images/events/PCB_seminar.jpg" alt="PCB Seminar">
-            <div class="carousel-caption">
-                <h3>PCB Seminar</h3>
-                <p>Learn the basics of PCB making from our very own Research and Development officers!
-                </p>
+            <div class="container">
+                <div class="slide-wrapper">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="slide-image">
+                                <div class="thumbnail">
+                                    <img src="images/events/PCB_seminar.jpg" alt="PCB Seminar">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="slide-content">
+                                <h1>PCB Seminar</h1>
+                                <p>Learn the basics of PCB making from our very own Research and Development officers!</p>
+                                <a href="a" class="btn-primary"> Link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div class="item">
-            <img src="images/events/DLSU_jobexpo.png" alt="DLSU Job Expo">
-            <div class="carousel-caption">
-                <h3>DLSU Job Expo</h3>
-                <p>Are you ready to SHIFT YOUR GEARS for the DLSU Job Expo 2015?
-                    The list of participating companies and organizations may be viewed here: http://tinyurl.com/JESEP15List</p>
-            </div>
-        </div>
-
-        <div class="item">
-            <img src="images/events/DOTA2_Tournament.jpg" alt="ACCESS DOTA 2 League">
-            <div class="carousel-caption">
-                <h3>ACCESS DOTA 2 League</h3>
-                <p>Coming SOON! Stay tuned for updates!</p>
-            </div>
-        </div>
-
-        <div class="item">
-            <img src="images/events/Frosh_convocation.jpg" alt="Frosh Convocation">
-            <div class="carousel-caption">
-                <h3>Frosh Convocation</h3>
-                <p>Grab the opportunity to know your professors who will be guiding and teaching you throughout your whole college life.</p>
-            </div>
-        </div>
-
     </div>
 
     <!-- Left and right controls -->
